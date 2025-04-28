be4615aab1d86e14442a48fee8ca74e655f88729
KieranFowler <kieran9fowler@gmail.com>
2025-04-28 10:41:28 +0100

    include UI kit react files/updated router/add alias paths to vite config.js/installed dependices for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route, Navigate, useRoutes } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import RtlLayout from "./layouts/rtl";
+import AdminLayout from "./layouts/admin";
+import AuthLayout from "./layouts/auth";
+const App = () => {
+  const routes = useRoutes([
+    { path: "auth/*", element: <AuthLayout /> },
+    { path: "admin/*", element: <AdminLayout /> },
+    { path: "rtl/*", element: <RtlLayout /> },
+    { path: "/", element: <Navigate to="/admin" replace /> },
+  ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  return routes;
+};
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, 'src/components'),
+      layouts: path.resolve(__dirname, 'src/layouts'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      assets: path.resolve(__dirname, 'src/assets'),
+      routes: path.resolve(__dirname, 'src/routes'),
+      views: path.resolve(__dirname, 'src/views'),
+      variables: path.resolve(__dirname, 'src/variables'),
+      // Add more aliases as needed assets/img/auth
+    },
+  },
+});
