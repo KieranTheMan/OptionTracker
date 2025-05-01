ead82a6c92a9ff52b47e9f2f4aa8cdcfb4a9f6fb
KieranFowler <kieran9fowler@gmail.com>
2025-05-01 11:29:21 +0100

    pass openModal to side bar/ include  manage Portfolio Section with Button to trigger Modal

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,12 +1,19 @@
 /* eslint-disable */
+import React from "react"
+import { HiX} from "react-icons/hi";
+// Icon Imports
+import {
+  MdHome,
+} from "react-icons/md";
 
-import { HiX } from "react-icons/hi";
 import Links from "./components/Links";
 
 import SidebarCard from "components/sidebar/componentsrtl/SidebarCard";
 import routes from  "routes";
 
-const Sidebar = ({ open, onClose }) => {
+const Sidebar = ( {open, onClose, openModal}) => {
+  
+
   return (
     <div
       className={`sm:none duration-175 linear fixed !z-50 flex min-h-full flex-col bg-white pb-10 shadow-2xl shadow-white/5 transition-all dark:!bg-navy-800 dark:text-white md:!z-50 lg:!z-50 xl:!z-0 ${
@@ -27,6 +34,28 @@ const Sidebar = ({ open, onClose }) => {
       </div>
       <div className="mt-[58px] mb-7 h-px bg-gray-300 dark:bg-white/30" />
       {/* Nav item */}
+
+      <ul className="mb-auto pt-1">
+        {/* Manage Portfolio Section */}
+      <div className="flex items-center justify-between mb-6">
+        <div className="flex items-center space-x-2">
+          <MdHome className="h-6 w-6" />
+          <h1 className="text-lg font-semibold">Manage Portfolio</h1>
+        </div>
+
+        {/* Button to open modal */}
+        <button
+          onClick= {()=>{
+            openModal();
+          console.log('button clicked')
+          }}
+          className="bg-blue-500 hover:bg-blue-600 text-white px-2 py-1 rounded"
+        >
+          ManPort
+        </button>
+</div>
+      </ul>
+
 
       <ul className="mb-auto pt-1">
         <Links routes={routes} />
